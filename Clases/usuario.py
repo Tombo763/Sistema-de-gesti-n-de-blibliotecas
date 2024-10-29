3a6094e9ae328ce985d4a7804fa6056bffdc2011
Tombo763 <olivaresalessandro10@gmail.com>
2024-10-29 19:11:58 -0300

    .

--- Clases/usuario.py
+++ Clases/usuario.py
@@ -1,8 +1,8 @@
-import tipo_usuario
+from tipo_usuario import Tipo_usuario
 
-class Usuario(tipo_usuario): #HERENCIA
+class Usuario(Tipo_usuario): #HERENCIA
     def __init__(self, idUsuario, nombre, direccion, telefono, correoElectronico, id_tipo_usuario):
-        tipo_usuario.Tipo_usuario.__init__(id_tipo_usuario)
+        Tipo_usuario.Tipo_usuario.__init__(id_tipo_usuario)
         super().__init__(id_tipo_usuario)
         self.idUsuario = idUsuario
         self.nombre = nombre
